7763ca51069f2a596f44e79169d5c0c03d7dbb19
wwju <wwju@yandex.ru>
2015-05-12 17:59:07 +0700

    move auth into another methods

--- lib/edit_i18n_database_translations/controller_module.rb
+++ lib/edit_i18n_database_translations/controller_module.rb
@@ -4,17 +4,23 @@ module EditI18nDatabaseTranslations
       params[:i18n_editor] == 'true'
     end
 
+    def i18n_editor_options
+      config = EditI18nDatabaseTranslations.config
+      {
+        name: config.user_name,
+        password: config.password,
+        if: :are_you_i18n_editor?
+      }
+    end
+
+    def i18n_editor_auth!
+      self.class.http_basic_authenticate_with i18n_editor_options
+    end
+
     def self.included(base)
       base.class_eval do
         helper_method :are_you_i18n_editor?
-
-        config = EditI18nDatabaseTranslations.config
-        options = {
-          name: config.user_name,
-          password: config.password,
-          if: :are_you_i18n_editor?
-        }
-        http_basic_authenticate_with options
+        before_action :i18n_editor_auth!
       end
     end
   end
