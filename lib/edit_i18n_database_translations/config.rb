81d0613905236c3bb10c9e16cda1084cceadcd35
wwju <wwju@yandex.ru>
2015-05-13 18:37:17 +0700

    added list of locales

--- lib/edit_i18n_database_translations/config.rb
+++ lib/edit_i18n_database_translations/config.rb
@@ -3,7 +3,16 @@ module EditI18nDatabaseTranslations
     attr_accessor :user_name, :password, :path_prefix, :allowed_keys
 
     def save_translation_path
-      EditI18nDatabaseTranslations::Engine.routes.url_helpers.i18n_editor_save_translation_path
+      url_helpers.i18n_editor_save_translation_path
+    end
+
+    def admin_path(options = {})
+      url_helpers.i18n_editor_admin_path(options)
+    end
+
+    private
+    def url_helpers
+      EditI18nDatabaseTranslations::Engine.routes.url_helpers
     end
   end
 end
